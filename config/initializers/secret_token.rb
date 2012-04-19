604f0d32fef6984240ee657bcadf8dcbd1bb9790
Jin-Sih Lin <linpct@gmail.com>
2012-04-19 11:21:10 +0800

    inti

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+P4::Application.config.secret_token = '520920749475f83aa215ef7a5c8cea569912f4af6887227b2b07354d0a5e13bb907783f0d20d83251bd1a553eb62ed25e6d0cf8fbbf71caaee97a8ebf979b691'
